f24ba5b190a497897d94d0e38a812b707c26a7d9
suzannakatseyedasilva <edu35.r@gmail.com>
2025-09-22 22:44:58 -0300

    outside quase pronta

--- TCCProject/Assets/TimelineController.cs
+++ TCCProject/Assets/TimelineController.cs
@@ -2,21 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using Cinemachine;
 
 public class TimelineController : MonoBehaviour
 {
     public PlayableDirector director;
     public GameObject playerController;
 
+    public CinemachineVirtualCamera scooterCamera;
+    public CinemachineVirtualCamera firstPersonCamera;
+
     void Start()
     {
         director.stopped += OnTimelineFinished;
+
+        // Desativa controle do jogador e define prioridade inicial
         playerController.SetActive(false);
+        scooterCamera.Priority = 20;
+        firstPersonCamera.Priority = 10;
+
         director.Play();
     }
 
     void OnTimelineFinished(PlayableDirector pd)
     {
+        // Ativa controle do jogador
         playerController.SetActive(true);
+
+        // Troca de prioridade para mudar a câmera
+        scooterCamera.Priority = 10;
+        firstPersonCamera.Priority = 20;
     }
 }
