5a8fa7fd81f8de638d1f0dd12c76bb2fcc8a6f0f
suzannakatseyedasilva <edu35.r@gmail.com>
2025-10-16 18:48:21 -0300

    dialogo programado

--- TCCProject/Assets/Codes/Game/Cutscenes/DialogoAcervo.cs
+++ TCCProject/Assets/Codes/Game/Cutscenes/DialogoAcervo.cs
@@ -14,6 +14,8 @@ public class DialogoAcervo : MonoBehaviour
     {
         if (!hasTriggered && other.CompareTag("Player"))
         {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
             hasTriggered = true;
             playerController.SetActive(false); // Desativa controle do jogador
             dialogueManager.SetActive(true); // Inicia diálogo
@@ -22,6 +24,8 @@ public class DialogoAcervo : MonoBehaviour
 
     public void EndCutscene()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         dialogueManager.SetActive(false);
         playerController.SetActive(true); // Reativa controle do jogador
         keyObject.SetActive(true); // Ativa a chave
